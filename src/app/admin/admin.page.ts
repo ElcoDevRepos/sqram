ce2cc74434ff6d2e0db756ca61ca2b22864807ca
Austin Hunter <ahunter8850@outlook.com>
2024-05-21 12:56:02 -0500

    minor

--- src/app/admin/admin.page.ts
+++ src/app/admin/admin.page.ts
@@ -1,9 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Auth, signInWithEmailAndPassword } from '@angular/fire/auth';
 import {
-  Auth,
-  signInWithEmailAndPassword,
-} from '@angular/fire/auth';
-import { Firestore, query, where, collection, getDocs, updateDoc, doc, addDoc, collectionData } from '@angular/fire/firestore';
+  Firestore,
+  query,
+  where,
+  collection,
+  getDocs,
+  updateDoc,
+  doc,
+  addDoc,
+  collectionData,
+} from '@angular/fire/firestore';
 import { Word } from '../classes/word';
 import * as dayjs from 'dayjs';
 import * as utc from 'dayjs/plugin/utc';
@@ -21,15 +28,19 @@ export class AdminPage implements OnInit {
   hint: string;
   username: string;
   password: string;
-  loggedIn: boolean = true;
-  chosenDate: string =dayjs(new Date().toISOString()).utc().toISOString();
+  loggedIn: boolean = false;
+  chosenDate: string = dayjs(new Date().toISOString()).utc().toISOString();
   words: Array<Word> = [];
   activeDoc: string;
   view: string = 'words';
   statWords: Array<any> = [];
   activeStats;
   activeGames;
-  constructor(private auth: Auth, private firestore: Firestore, private alertCtrl: AlertController) { }
+  constructor(
+    private auth: Auth,
+    private firestore: Firestore,
+    private alertCtrl: AlertController
+  ) {}
 
   ngOnInit() {
     this.getWords();
@@ -39,19 +50,23 @@ export class AdminPage implements OnInit {
 
   async getWords() {
     this.words = [];
-    let ref = collection(
-      this.firestore,
-      'words',
+    let ref = collection(this.firestore, 'words');
+    let q = query(
+      ref,
+      where(
+        'date',
+        '==',
+        dayjs(new Date(this.chosenDate).toISOString()).format('MM/DD/YY')
+      )
     );
-    let q = query(ref, where("date", '==', dayjs(new Date(this.chosenDate).toISOString()).format('MM/DD/YY')));
     const docs = await getDocs(q);
     let words = [];
     if (docs.size == 0) {
       this.activeDoc = null;
-      addDoc(collection(this.firestore, "words"), {
+      addDoc(collection(this.firestore, 'words'), {
         date: dayjs(this.chosenDate).format('MM/DD/YY'),
         words: [],
-        todays_top_10: []
+        todays_top_10: [],
       });
       this.getWords();
     } else {
@@ -61,23 +76,23 @@ export class AdminPage implements OnInit {
           words.push(w);
         });
         words.sort((a, b) => {
-          if (a['index'] > b['index']) {    
-            return 1;    
-          } else if (a['index'] < b['index']) {    
-              return -1;    
-          }    
-          return 0; 
-        })
+          if (a['index'] > b['index']) {
+            return 1;
+          } else if (a['index'] < b['index']) {
+            return -1;
+          }
+          return 0;
+        });
         this.words = words;
-      })
+      });
     }
   }
 
   async getStats() {
-    const docs = await getDocs(collection(this.firestore, "words"));
+    const docs = await getDocs(collection(this.firestore, 'words'));
     docs.forEach((doc) => {
       this.statWords.push(doc.data());
-    })
+    });
 
     for (let i = 0; i < this.statWords.length; i++) {
       let date = dayjs(this.chosenDate).utc().format('MM/DD/YY');
@@ -91,16 +106,13 @@ export class AdminPage implements OnInit {
   }
 
   async getActiveGames() {
-    let ref = collection(
-      this.firestore,
-      'users',
-    );
-    let q = query(ref, where("active", '==', true));
+    let ref = collection(this.firestore, 'users');
+    let q = query(ref, where('active', '==', true));
     const docs = await collectionData(q);
     docs.subscribe((data) => {
       let count = 0;
       let today = dayjs().utc();
-      console.log()
+      console.log();
       data.forEach((d) => {
         let gameDate = dayjs(d.lastPlayed).utc();
         if (today.isSame(gameDate, 'day')) {
@@ -109,15 +121,14 @@ export class AdminPage implements OnInit {
           console.log(today.diff(gameDate, 'hours'));
           count++;
         }
-      })
+      });
 
       this.activeGames = count;
-    })
+    });
   }
 
   calculateStats() {
     console.log(this.words);
-    
   }
 
   async login() {
@@ -133,24 +144,20 @@ export class AdminPage implements OnInit {
     return true;
   }
   async submit() {
-    if (!this.isValidWord()) return
-    
+    if (!this.isValidWord()) return;
+
     if (this.activeDoc) {
-      let ref = doc(
-        this.firestore,
-        'words',
-        this.activeDoc
-      );
+      let ref = doc(this.firestore, 'words', this.activeDoc);
       let temp = [...this.words];
       temp.push({
         index: this.words.length,
         text: this.word,
-        hint: this.hint
+        hint: this.hint,
       });
-  
+
       await updateDoc(ref, {
-        words: temp
-      })
+        words: temp,
+      });
     }
     this.word = '';
     this.hint = '';
@@ -164,14 +171,10 @@ export class AdminPage implements OnInit {
     this.words[ev.detail.to].index = ev.detail.from;
     this.words[ev.detail.from].index = ev.detail.to;
 
-    let ref = doc(
-      this.firestore,
-      'words',
-      this.activeDoc
-    );
+    let ref = doc(this.firestore, 'words', this.activeDoc);
     updateDoc(ref, {
-      words: this.words
-    })
+      words: this.words,
+    });
     // Finish the reorder and position the item in the DOM based on
     // where the gesture ended. This method can also be called directly
     // by the reorder group
@@ -191,22 +194,18 @@ export class AdminPage implements OnInit {
   async delete(index) {
     let temp = [...this.words];
     temp.splice(index, 1);
-    let ref = doc(
-      this.firestore,
-      'words',
-      this.activeDoc
-    );
+    let ref = doc(this.firestore, 'words', this.activeDoc);
 
     await updateDoc(ref, {
-      words: temp
-    })
+      words: temp,
+    });
 
     this.getWords();
   }
 
   async edit(index) {
     const alert = await this.alertCtrl.create({
-      header: 'Please enter the word\'s info',
+      header: "Please enter the word's info",
       buttons: [
         {
           text: 'Submit',
@@ -217,15 +216,11 @@ export class AdminPage implements OnInit {
             temp[index].hint = data[1];
             temp[index].text = data[0];
 
-            let ref = doc(
-              this.firestore,
-              'words',
-              this.activeDoc
-            );
-        
+            let ref = doc(this.firestore, 'words', this.activeDoc);
+
             await updateDoc(ref, {
-              words: temp
-            })
+              words: temp,
+            });
             this.getWords();
           },
         },
@@ -251,4 +246,3 @@ export class AdminPage implements OnInit {
     await alert.present();
   }
 }
-
